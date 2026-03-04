0263a3ff1bb33d8dea5276f33aa1c589c8ae131b
devhas-cloud <devhas01@gmail.com>
2026-03-04 10:19:31 +0700

    timeout send
    agar tidak hang

--- klhk/send.py
+++ klhk/send.py
@@ -77,7 +77,7 @@ def write_log(message):
 def get_jwt_token():
     global API_JWT
     try:
-        response = requests.get(API_JWT)
+        response = requests.get(API_JWT, timeout=(5, 15))
         if response.status_code == 200:
             jwt_token = response.text.strip()
             if jwt_token:
@@ -161,7 +161,7 @@ def send_data_to_api(data, start, end):
             return
 
         headers = {'Authorization': f'Bearer {key_token}', 'Content-Type': 'application/json'}
-        response = requests.post(API_ENDPOINT, json={"token": encoded}, headers=headers)
+        response = requests.post(API_ENDPOINT, json={"token": encoded}, headers=headers, timeout=(5, 30))
         result = response.json()
 
         write_log(f"API Response : {response.text}")
